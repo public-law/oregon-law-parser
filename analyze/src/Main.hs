b0f70505684f4ce42363b660017e0f6484dc8407
Robb Shecter <robb@weblaws.org>
2016-08-08 22:52:08 -0700

    Parse references

--- analyze/src/Main.hs
+++ analyze/src/Main.hs
@@ -7,19 +7,21 @@ module Main where
 import           Data.Aeson
 import           Data.Aeson.Encode.Pretty
 import           Data.ByteString.Lazy     (putStr)
-import           Data.List                (isPrefixOf)
+import           Data.List                (isPrefixOf, nub, sort)
 import           Data.Maybe               (fromMaybe)
 import           GHC.Generics
 import           Prelude.Unicode
 import           System.Environment       (getArgs)
 import           Text.HandsomeSoup
+import           Text.Regex.Posix
 import           Text.XML.HXT.Core
 
 
 
 data Amendment =
   Amendment {
-      summary ∷ String
+      summary   ∷ String,
+      citations ∷ [String]
     } deriving (Show, Generic)
 
 instance ToJSON Amendment
@@ -38,11 +40,16 @@ main = do
 
 makeAmendment ∷ String → Amendment
 makeAmendment html =
-  Amendment {
-    summary = html
-                |> paragraphs
-                |> findSummary
-                |> fromMaybe "(Summary is not available)"
+  let lines = paragraphs html
+  in Amendment {
+    summary = lines
+      |> findSummary
+      |> fromMaybe "(Summary is not available)",
+    citations = lines
+      |> (map sectionNumbers)
+      |> concat
+      |> nub
+      |> sort
   }
 
 
@@ -57,6 +64,12 @@ findSummary text =
   case filter isSummary text of
     [x] → Just x
     _   → Nothing
+
+
+sectionNumbers ∷ String → [String]
+sectionNumbers text =
+  let pattern = "[0-9][0-9][0-9]\\.[0-9][0-9][0-9]" ∷ String
+  in getAllTextMatches $ text =~ pattern ∷ [String]
 
 
 isSummary ∷ String → Bool
