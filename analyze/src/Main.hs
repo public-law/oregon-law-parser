3bd883bb5d3dc3d716f9a92d44c59237541dac3f
Robb Shecter <robb@weblaws.org>
2016-08-08 20:14:38 -0700

    Moved the code over

--- analyze/src/Main.hs
+++ analyze/src/Main.hs
@@ -1,5 +1,58 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE UnicodeSyntax     #-}
+
 module Main where
 
-main :: IO ()
+import           Data.Aeson
+import           Data.Aeson.Encode.Pretty
+import           Data.ByteString.Lazy     (putStr)
+import           Data.List                (isPrefixOf)
+import           Data.Maybe               (fromMaybe)
+import           GHC.Generics
+import           Prelude.Unicode
+import           System.Environment       (getArgs)
+import           Text.HandsomeSoup
+import           Text.XML.HXT.Core
+
+
+
+data Amendment =
+  Amendment {
+      summary ∷ String,
+      text    ∷ [String]
+    } deriving (Show, Generic)
+
+instance ToJSON Amendment
+
+
+main ∷ IO ()
 main = do
-  putStrLn "hello world"
+  html ← getContents
+  let amendment = newAmendment html
+  Data.ByteString.Lazy.putStr (encodePretty amendment)
+
+
+newAmendment ∷ String → Amendment
+newAmendment html =
+  Amendment {
+    summary = fromMaybe "No summary found" (findSummary (paragraphs html)),
+    text    = paragraphs html
+  }
+
+
+paragraphs ∷ String → [String]
+paragraphs html =
+  runLA (hread >>> css "p" //> getText) html
+
+
+findSummary ∷ [String] → Maybe String
+findSummary text =
+  case filter isSummary text of
+    [x] → Just x
+    _   → Nothing
+
+
+isSummary ∷ String → Bool
+isSummary sentence =
+  "Relating to" `isPrefixOf` sentence
