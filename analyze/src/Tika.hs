adc40eda6ef9620fa090f1911754a14fc0a73b27
Robb Shecter <robb@weblaws.org>
2016-08-17 23:28:12 -0700

    refactoring

--- analyze/src/Tika.hs
+++ analyze/src/Tika.hs
@@ -9,11 +9,13 @@ import           System.Process    (readProcessWithExitCode)
 import           Text.HandsomeSoup
 import           Text.XML.HXT.Core (getText, hread, runLA, (//>), (>>>))
 
+jarFileLocation = "/Users/robb/lib/tika-app.jar"
+
 
 
 runTika ∷ String → IO (ExitCode, String, String)
 runTika pdfFilename =
-  readProcessWithExitCode "java" ["-jar", "/Users/robb/lib/tika-app.jar", "--html", pdfFilename] ""
+  readProcessWithExitCode "java" ["-jar", jarFileLocation, "--html", pdfFilename] ""
 
 
 paragraphs ∷ String → [String]
