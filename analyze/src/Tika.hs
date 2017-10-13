dc1aff16a13d116b5641fba7111fc611df3d32a7
Joe Hillenbrand <joehillen@gmail.com>
2017-10-12 23:55:10 -0700

    use java's CLASSPATH to find tika-app.jar
    - it makes sense to look in the current directory for tika-app.jar by default
    - use lts-8.6 because it compiles and stack's 7.10.3 doesn't work on archlinux
    
    Fixes #1

--- analyze/src/Tika.hs
+++ analyze/src/Tika.hs
@@ -5,18 +5,25 @@ module Tika where
 import           Data.List         (isPrefixOf)
 import           GHC.IO.Exception
 import           Prelude.Unicode
+import           System.Environment (lookupEnv)
 import           System.Process    (readProcessWithExitCode)
 import           Text.HandsomeSoup
 import           Text.XML.HXT.Core (getText, hread, runLA, (//>), (>>>))
 
-jarFile        = "/Users/robb/lib/tika-app.jar"
-javaExecutable = "java"
+jarFile ∷ String
+jarFile = "tika-app.jar"
 
+java ∷ [String] → IO (ExitCode, String, String)
+java args = readProcessWithExitCode "java" args ""
 
 
 runTika ∷ String → IO (ExitCode, String, String)
-runTika pdfFilename =
-  readProcessWithExitCode javaExecutable ["-jar", jarFile, "--html", pdfFilename] ""
+runTika pdfFilename = do
+  classpath <- lookupEnv "CLASSPATH"
+  let args = ["--html", pdfFilename]
+  case classpath of
+    Nothing -> java ("-jar":jarFile:args)
+    Just _  -> java ("org.apache.tika.cli.TikaCLI":args)
 
 
 paragraphs ∷ String → [String]
