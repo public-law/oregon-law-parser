f93a7d48697562b5d2180a602ac883468463478e
Robb Shecter <robb@weblaws.org>
2020-01-12 22:15:18 -0800

    fix the regex for citation

--- analyze/src/Amendment.hs
+++ analyze/src/Amendment.hs
@@ -55,21 +55,21 @@ makeBill citation =
 
 findCitation ∷ [String] → String
 findCitation phrases =
-  let match = phrases
+  let maybeMatch = phrases
         & join
-        & firstMatch "(HB|SB) [0-9]{4}"
-  in case(match) of
+        & firstMatch "(HB|SB) [0-9]+"
+  in case(maybeMatch) of
     Just s -> s
     Nothing -> error ("Could not find citation in " ++ (show phrases))
 
 
 findYear ∷ [String] → Integer
 findYear input =
-  let match = input
+  let maybeMatch = input
         & join
         & firstMatch "OREGON LAWS [0-9]{4}"
 
-  in case(match) of
+  in case(maybeMatch) of
     Just s -> s
               & splitWs -- I don't know how to capture a group yet
               & last
@@ -79,12 +79,12 @@ findYear input =
 
 findChapter ∷ [String] → Integer
 findChapter phrases =
-  let match = 
+  let maybeMatch =
         phrases
         & join
         & firstMatch "Chap. [0-9]{1,3}"
 
-  in case(match) of
+  in case(maybeMatch) of
     Just s -> s
               & splitWs
               & last
@@ -94,10 +94,13 @@ findChapter phrases =
 
 findEffectiveDate ∷ [String] → Day
 findEffectiveDate paragraphs =
-  paragraphs
-    & join
-    & firstMatch "Effective date .+ [0-9]+, [0-9]{4}"
-    & parseTimeOrError True defaultTimeLocale "Effective date %B %-d, %Y"
+  let maybeMatch =
+        paragraphs
+        & join
+        & firstMatch "Effective date .+ [0-9]+, [0-9]{4}"
+  in case (maybeMatch) of
+    Just s -> parseTimeOrError True defaultTimeLocale "Effective date %B %-d, %Y" s
+    Nothing -> error ("Could not find Effective Date in " ++ (show paragraphs))
 
 
 findSummary ∷ [String] → String
